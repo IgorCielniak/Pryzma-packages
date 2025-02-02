41ef539ed26b26c5b2178f65b8ff5eb4b7024d38
IgorCielniak <igorcielniak.contact@gmail.com>
2025-02-02 19:32:10 +0100

    updated the net library

--- net/net.py
+++ net/net.py
@@ -1,14 +1,14 @@
-def ping(variables, url):
+def ping(self, url):
     import os
     os.system("ping " + url)
 
-def download(variables, url):
+def download(self, url):
     import urllib.request
     response = urllib.request.urlopen(url)
     response = response.read()
-    variables["response"] = response
+    self.variables["response"] = response
 
-def http_post(variables, url, data):
+def http_post(self, url, data):
     import requests
     response = requests.post(url, json = data)             
-    variables["response"] = response
+    self.variables["response"] = response
